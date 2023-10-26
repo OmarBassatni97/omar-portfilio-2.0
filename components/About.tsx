9ed3e3a0e69614ec9b85a4757ab7e6e22b7f4ebe
Omar Bassatni <omarbassatni@gmail.com>
2023-10-26 13:35:06 +0300

    adding home and about section

---
+++ components/About.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import SectionHeading from './SectionHeading'
+
+const About = () => {
+    return (
+        <section className='mb-28 max-w-[45rem] text-center leading-8 sm:mb-40'>
+            <SectionHeading>About me</SectionHeading>
+            <p className='mb-3'>👋Hello, I'm Omar Bassatni, a dedicated Frontend Developer with a strong passion for crafting intuitive and visually appealing digital experiences. With a solid foundation in React.js and Next.js, I specialize in turning creative ideas into functional and user-friendly web applications.</p>
+            <p className='mb-3'>🌟 Passionate about continuous learning, I thrive on staying up-to-date with the latest trends and best practices in frontend development. I believe that great code is born from a combination of creativity and precision, and I'm dedicated to achieving that balance in every project I undertake.</p>
+        </section>
+    )
+}
+
+export default About
