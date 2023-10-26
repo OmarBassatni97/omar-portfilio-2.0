3421fb75c19c01e62876a72126380ab890f51240
Omar Bassatni <omarbassatni@gmail.com>
2023-10-26 13:43:58 +0300

    animated about section

--- components/About.tsx
+++ components/About.tsx
@@ -1,13 +1,19 @@
+'use client'
 import React from 'react'
 import SectionHeading from './SectionHeading'
-
+import { motion } from 'framer-motion'
 const About = () => {
     return (
-        <section className='mb-28 max-w-[45rem] text-center leading-8 sm:mb-40'>
+        <motion.section
+            className='mb-28 max-w-[45rem] text-center leading-8 sm:mb-40'
+            initial={{ opacity: 0, y: 100 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ delay: 0.175 }}
+        >
             <SectionHeading>About me</SectionHeading>
             <p className='mb-3'>👋Hello, I'm Omar Bassatni, a dedicated Frontend Developer with a strong passion for crafting intuitive and visually appealing digital experiences. With a solid foundation in React.js and Next.js, I specialize in turning creative ideas into functional and user-friendly web applications.</p>
             <p className='mb-3'>🌟 Passionate about continuous learning, I thrive on staying up-to-date with the latest trends and best practices in frontend development. I believe that great code is born from a combination of creativity and precision, and I'm dedicated to achieving that balance in every project I undertake.</p>
-        </section>
+        </motion.section>
     )
 }
 
