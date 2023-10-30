ff06717512bfff395f07c10f37b5061f9ed470ad
Omar Bassatni <omarbassatni@gmail.com>
2023-10-30 20:31:08 +0200

    adding id to sections

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -5,17 +5,17 @@ import { projectsData } from '@/lib/data'
 import Project from './Project'
 const Projects = () => {
   return (
-    <section>
-        <SectionHeading>My Projects</SectionHeading>
-        <div>
-            {
-                projectsData.map((project,index)=>(
-                   <Project key={index} {...project}/> 
-                )
+    <section id='projects' className='scroll-mt-28'>
+      <SectionHeading>My Projects</SectionHeading>
+      <div>
+        {
+          projectsData.map((project, index) => (
+            <Project key={index} {...project} />
+          )
 
-                )
-            }
-        </div>
+          )
+        }
+      </div>
     </section>
   )
 }
