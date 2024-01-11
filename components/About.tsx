d59c23e470db8e4d472cab7c00648f8a8924b327
Omar Bassatni <omarbassatni@gmail.com>
2024-01-11 15:53:03 +0200

    Cleaning code

--- components/About.tsx
+++ components/About.tsx
@@ -1,9 +1,7 @@
 'use client'
-import React, { useEffect } from 'react'
+import React from 'react'
 import SectionHeading from './SectionHeading'
 import { motion } from 'framer-motion'
-import { useInView } from 'react-intersection-observer'
-import { useActiveSectionContext } from '@/context/active-section-context'
 import { useSectionInView } from '@/lib/hooks'
 const About = () => {
     const { ref } = useSectionInView("About")
--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,11 +1,9 @@
 "use client"
-import React, { useEffect } from 'react'
+import React from 'react'
 import SectionHeading from './SectionHeading'
-import { motion } from 'framer-motion'
+
 import { projectsData } from '@/lib/data'
 import Project from './Project'
-import { useActiveSectionContext } from '@/context/active-section-context'
-import { useInView } from 'react-intersection-observer'
 import { useSectionInView } from '@/lib/hooks'
 const Projects = () => {
   const {ref} = useSectionInView("Projects",0.5)
